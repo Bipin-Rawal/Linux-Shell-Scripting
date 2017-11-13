0f1e1f940b9e412496aaef5a80c1da60fb57129a
Bipin Rawal <bipinrawal81@gmail.com>
2017-11-13 21:49:30 +0530

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,5 +1,9 @@
-echo "enter your name,enrollno,program"
-read myname enroll pro
+echo "enter your name"
+read myname
+echo "enter your enroll"
+read enroll
+echo "enter your program name"
+read pro
 echo "your name is:"$myname
 echo "enrollment nme is:"$enroll
 echo "your program is:"$pro
